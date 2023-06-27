c3a033ef5a8f2c5dd40a5110dd0de0ae08d23423
devwjdalsdl07 <dev.wjdalsdl07@gmail.com>
2023-06-27 14:37:43 +0900

    feat:  Header & Footer 라우터 및 Css 작업

---
+++ src/style/HeaderCss.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Wrap = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  background: #82c32e;
+  font-size: 15px;
+  a {
+    color: #fff;
+    font-weight: 400;
+  }
+`;
