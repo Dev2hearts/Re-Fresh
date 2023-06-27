bed9f7dfd6b842fc3e860717416b8c359affe854
dev2hearts <js.park119de@gmail.com>
2023-06-27 14:37:43 +0900

    feat: calendar 1차 작업 완료

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,12 +1,55 @@
-import { Alert, Calendar } from "antd";
+import { Alert, Calendar, Modal } from "antd";
 import dayjs from "dayjs";
 import { useState } from "react";
 import locale from "antd/es/calendar/locale/ko_KR";
+import "../style/schedule.css";
+import moment from "moment";
 
 const Schedule = () => {
+  const hi = [
+    { date: "2023-06-20" },
+    { date: "2023-06-03" },
+    { date: "2023-06-10" },
+    { date: "2023-06-29" },
+  ];
   const [value, setValue] = useState(() => dayjs(Date.now()));
   const [selectedValue, setSelectedValue] = useState(() => dayjs(Date.now()));
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const cellRender = date => {
+    const dateString = date.format("YYYY-MM-DD");
+
+    const result = hi.find(item => {
+      if (item.date === dateString) {
+        return item;
+      }
+    });
+    if (result) {
+      return <div>{"장보는날"}</div>;
+    }
+    return null;
+  };
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   const onSelect = newValue => {
+    setValue(newValue);
+    setSelectedValue(newValue);
+    const dateString = newValue.format("YYYY-MM-DD");
+    const result = hi.find(item => {
+      if (item.date === dateString) {
+        return item;
+      }
+    });
+    if (!result) {
+      showModal();
+    }
     setValue(newValue);
     setSelectedValue(newValue);
   };
@@ -15,15 +58,20 @@ const Schedule = () => {
   };
   return (
     <div>
+      <Modal
+        title={`${selectedValue?.format("YYYY-MM-DD")}`}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      ></Modal>
       <Calendar
         locale={locale}
         value={value}
         onSelect={onSelect}
         onPanelChange={onPanelChange}
+        cellRender={cellRender}
       />
-      <Alert
-        message={`오늘의 날짜: ${selectedValue?.format("YYYY-MM-DD")}`}
-      />
+      <Alert message={`${selectedValue?.format("YYYY-MM-DD")}`} />
     </div>
   );
 };
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,17 +2,18 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer"
 import Schedule from "../components/Schedule"
+import { MainWrap } from "../style/MainCss";
 
 
 const Main = () => {
   return (
-    <div>
+    <MainWrap>
       <Header />
       <div>
         <Schedule />
       </div>
       <Footer />
-    </div>
+    </MainWrap>
   );
 };
 
