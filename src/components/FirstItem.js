f1d750352a47a77a7b61d1904a0310cc367c2bf7
devwjdalsdl07 <dev.wjdalsdl07@gmail.com>
2023-06-30 19:00:01 +0900

    bug fix

--- src/components/FirstItem.js
+++ src/components/FirstItem.js
@@ -4,17 +4,26 @@ import { ItemBox } from "../style/FirstItemCss";
 import { InputNumber, Input, Select } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { getCate, getUnit } from "../api/fetch";
 
-const FirstItem = ({ onDelete, index, cateList }) => {
+// 추가 코드 item
+const FirstItem = ({ onDelete, index, item }) => {
+  const [cateList, setCateList] = useState([]);
+  const [unitList, setUnitList] = useState([]);
   const [isEdit, setIsEdit] = useState(true);
+  const [selecCate, setSelecCate] = useState(null);
+
+  // 추가 코드 : Select 의 value 값
+  const [selectValue, setSelectValue] = useState(null);
+
+  const [itemName, setItemName] = useState();
+  const [selecUnit, setSelecUnit] = useState(null);
+  const [ea, setEa] = useState();
   const handleSaveClick = () => {
     setIsEdit(false);
   };
   const handleEditClick = () => {
     setIsEdit(true);
-  };
-  const handleChange = value => {
-    console.log(`Selected: ${value}`);
   };
   const handleCancelClick = () => {
     setIsEdit(false);
@@ -22,30 +31,39 @@ const FirstItem = ({ onDelete, index, cateList }) => {
   const handleRemoveClick = () => {
     onDelete(index);
   };
+  const handleCateChange = value => {
+    setSelecCate(value);
+    // 추가코드 사용자가 Selec 에서 값을 선택한 경우
+    setSelectValue(cateList[value].label);
+  };
+  const handleItemNameChange = e => {
+    setItemName(e.target.value);
+  };
+  const handleEaChange = e => {
+    setEa(e.target.value);
+  };
+  const handleUnitChange = value => {
+    setSelecUnit(value);
+  };
 
-  const handleCateChange = (value) => {
-    setSelecCate(value)
-  }
-  const handleItemNameChange = (e) => {
-    setItemName(e.target.value)
-  }
-  const handleEaChange = (e) => {
-    setEa(e.target.value)
-  }
-  const handleUnitChange = (value) => {
-    setSelecUnit(value)
-  }
-  const fetchCateData = async () => {
-    const result = await getCate();
-    setCateList(result);
-  };
-  const fetchUnitData = async () => {
-    const result = await getUnit();
-    setUnitList(result);
-  };
+  // 추가한 코드 item 이 넘어온다.
   useEffect(() => {
-    fetchCateData();
-    fetchUnitData();
+    // console.log("아이템 넘어옴");
+    if (!item.icate) {
+      setSelectValue("카테고리");
+      setIsEdit(true);
+    }
+  }, [item]);
+
+  useEffect(() => {
+    const fetchCateData = async () => {
+      const result = await getCate();
+      setCateList(result);
+    };
+    const fetchUnitData = async () => {
+      const result = await getUnit();
+      setUnitList(result);
+    };
     if (isEdit) {
       // 모달 창이 열릴 때마다 데이터를 초기화
       fetchCateData();
@@ -53,18 +71,18 @@ const FirstItem = ({ onDelete, index, cateList }) => {
     }
   }, [isEdit]);
 
-
   if (isEdit) {
     // 편집중
     return (
       <ItemBox>
         <div className="flex justify-between">
           <Select
+            value={selectValue}
             defaultValue="카테고리"
             style={{
               width: 205,
             }}
-            onChange={handleChange}
+            onChange={handleCateChange}
             disabled={false}
             options={cateList}
           />
@@ -74,26 +92,22 @@ const FirstItem = ({ onDelete, index, cateList }) => {
         </div>
 
         <div className="flex flex-wrap gap-1">
-          <Input placeholder="구매 목록" disabled={false} />
-          <InputNumber className="flex-1" defaultValue={1} disabled={false} />
+          <Input
+            placeholder="구매 목록"
+            disabled={false}
+            onChange={handleItemNameChange}
+          />
+          <InputNumber
+            className="flex-1"
+            defaultValue={1}
+            disabled={false}
+            onChange={handleEaChange}
+          />
           <Select
             className="flex-1"
             defaultValue="단위"
-            onChange={handleChange}
-            options={[
-              {
-                value: "ea",
-                label: "ea",
-              },
-              {
-                value: "kg",
-                label: "kg",
-              },
-              {
-                value: "g",
-                label: "g",
-              },
-            ]}
+            onChange={handleUnitChange}
+            options={unitList}
           />
         </div>
         <div className="flex justify-around">
@@ -112,11 +126,12 @@ const FirstItem = ({ onDelete, index, cateList }) => {
       <ItemBox onClick={handleEditClick}>
         <div className="flex justify-between">
           <Select
-            defaultValue="카테고리"
+            labelInValue
+            defaultValue={item.icate ? "gogo" : "카테고리"}
             style={{
               width: 205,
             }}
-            onChange={handleChange}
+            onChange={handleCateChange}
             disabled={true}
             // options={cateList}
           />
@@ -125,32 +140,23 @@ const FirstItem = ({ onDelete, index, cateList }) => {
           </button>
         </div>
         <div className="flex flex-wrap gap-1">
-          <Input placeholder="구매 목록" disabled={true} />
+          <Input
+            placeholder="구매 목록"
+            disabled={true}
+            onChange={handleItemNameChange}
+          />
           <InputNumber
             className="flex-1"
             defaultValue={1}
-            style={{ width: 70 }}
             disabled={true}
+            onChange={handleEaChange}
           />
           <Select
             className="flex-1"
             disabled={true}
             defaultValue="단위"
-            onChange={handleChange}
-            options={[
-              {
-                value: "ea",
-                label: "ea",
-              },
-              {
-                value: "kg",
-                label: "kg",
-              },
-              {
-                value: "g",
-                label: "g",
-              },
-            ]}
+            onChange={handleUnitChange}
+            options={unitList}
           />
         </div>
         <div className="flex justify-around"></div>
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -7,7 +7,6 @@ import FirstItem from "./FirstItem";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping, faPlus } from "@fortawesome/free-solid-svg-icons";
 
-// 손정민 추가 코드(쇼핑리스트 출력 : state 전달)
 const Schedule = ({ setOpenShopList, setOpenShopListDate, openShopList }) => {
   const hi = [{ date: "2023-06-29" }, { date: "2023-07-07" }];
   const [value, setValue] = useState(() => dayjs(Date.now()));
@@ -15,21 +14,16 @@ const Schedule = ({ setOpenShopList, setOpenShopListDate, openShopList }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [items, setItems] = useState([]);
 
-  const cellRender = date => {
-    const dateString = date.format("YYYY-MM-DD");
-    const result = hi.find(item => {
-      if (item.date === dateString) {
-        return item;
-      }
-    });
-    if (result) {
-      return (
-        <div className="text-right text-gray-500">
-          {<FontAwesomeIcon icon={faCartShopping} />}
-        </div>
-      );
-    }
-    return null;
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+    setItems([
+      {
+        icate: "",
+        iunit: "",
+        nm: "",
+        cnt: 1,
+      },
+    ]); // 모달 열 때 아이템 초기화
   };
   const handleOk = () => {
     setIsModalOpen(false);
@@ -42,77 +36,57 @@ const Schedule = ({ setOpenShopList, setOpenShopListDate, openShopList }) => {
   };
 
   const handleAddItem = () => {
-    // setItems(prevItems => [...prevItems, <FirstItem onDelete={() => handleDeleteItem(prevItems.length)} key={prevItems.index}/>]);
-    setItems([...items, {}]);
+    setItems(prevItems => [...prevItems, {}]); // 새로운 아이템 추가
   };
-  const [cateList, setCateList] = useState([
-    {
-      value: "냉동식품",
-      label: "냉동식품",
-    },
-    {
-      value: "과일/채소",
-      label: "과일/채소",
-    },
-    {
-      value: "유제품",
-      label: "유제품",
-    },
-  ]);
-  // 추가
-  const onDelete = _id => {
-    const newItemArr = items.filter((item, index) => index !== _id);
-    setItems(newItemArr);
+
+  const onDelete = index => {
+    setItems(prevItems => prevItems.filter((_, i) => i !== index)); // 아이템 삭제
   };
-  const handleModalOpen = () => {
-    setIsModalOpen(true);
-    setItems([{}]);
-  };
+
   const onSelect = newValue => {
     setValue(newValue);
     setSelectedValue(newValue);
     const dateString = newValue.format("YYYY-MM-DD");
-    const result = hi.find(item => {
-      if (item.date === dateString) {
-        return item;
-      }
-    });
-    // 손정민 추가 코드(목록이 펼쳐진 경우 구분)
-    if (openShopList) {
+    const result = hi.find(item => item.date === dateString);
+
+    if ((openShopList && !result) || !result) {
+      handleModalOpen(); // 모달 열기
+      setOpenShopListDate("");
       setOpenShopList(false);
-      if (!result) {
-        handleModalOpen();
-        // 손정민 추가 코드(쇼핑리스트 출력)
-        setOpenShopListDate("");
-        setOpenShopList(false);
-      }
     } else {
-      if (!result) {
-        handleModalOpen();
-        // 손정민 추가 코드(쇼핑리스트 출력)
-        setOpenShopListDate("");
-        setOpenShopList(false);
-      } else {
-        // 손정민 추가 코드(쇼핑리스트 출력)
-        setOpenShopList(true);
-      }
+      setOpenShopList(true);
     }
 
-    setValue(newValue);
-    setSelectedValue(newValue);
-    // 손정민 추가 코드(쇼핑리스트 출력)
     setOpenShopListDate(newValue.format("YYYY/MM/DD"));
   };
+
   const onPanelChange = newValue => {
     setValue(newValue);
   };
+
   useEffect(() => {
     const th = document.querySelectorAll(".ant-picker-content th");
     const day = ["일", "월", "화", "수", "목", "금", "토"];
     th.forEach((item, index) => {
-      item.innerHTML = day[index];
+      item.innerHTML = day[index]; // 캘린더 요일 텍스트 설정
     });
   }, []);
+
+  const cellRender = date => {
+    const dateString = date.format("YYYY-MM-DD");
+    const result = hi.find(item => item.date === dateString);
+
+    if (result) {
+      return (
+        <div className="text-right text-gray-500">
+          {<FontAwesomeIcon icon={faCartShopping} />}
+        </div>
+      );
+    }
+
+    return null;
+  };
+
   return (
     <div>
       <Modal
@@ -130,16 +104,18 @@ const Schedule = ({ setOpenShopList, setOpenShopListDate, openShopList }) => {
         >
           <FontAwesomeIcon icon={faPlus} />
         </button>
-        {items.map((item, index) => (
-          // <div key={index}>{item}</div>
-          // <FirstItem onDelete={() => handleDeleteItem(prevItems.length)} key={prevItems.index}/>
-          <FirstItem
-            key={index}
-            onDelete={onDelete}
-            index={index}
-            cateList={cateList}
-          />
-        ))}
+        {isModalOpen && (
+          <div>
+            {items.map((item, index) => (
+              <FirstItem
+                key={index}
+                onDelete={onDelete}
+                index={index}
+                item={item}
+              />
+            ))}
+          </div>
+        )}
       </Modal>
       <Calendar
         locale={locale}
@@ -148,9 +124,7 @@ const Schedule = ({ setOpenShopList, setOpenShopListDate, openShopList }) => {
         onPanelChange={onPanelChange}
         cellRender={cellRender}
       />
-      <div style={openShopList ? { display: "none" } : { display: "block" }}>
-        {selectedValue?.format("YYYY-MM-DD")}
-      </div>
+      {!openShopList && <div>{selectedValue?.format("YYYY-MM-DD")}</div>}
     </div>
   );
 };
